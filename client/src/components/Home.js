c25a79935a87b489fc60d12c6cb45f40000681f4
NichenametlaPranathi <pranathi1305@gmail.com>
2025-09-03 22:42:54 +0530

    the server is working

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-      <h1 className="text-5xl font-bold mb-4">Hi, I'm Vasavi </h1>
+      <h1 className="text-5xl font-bold mb-4">Hi, I'm N Pranathi </h1>
       <p className="text-lg">Full Stack Developer | MERN Stack Enthusiast</p>
     </div>
   );
